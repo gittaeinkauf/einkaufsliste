49e9b976cdf9dbaccc5b6705a00a734beaf4e03d
gittaeinkauf <falko.deutsch@gmx.de>
2026-09-06 10:04:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,10 @@
-const C='einkaufsliste-v2';const F=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+const CACHE="einkaufsliste-modern-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET")return;
+ e.respondWith(fetch(e.request).then(r=>{
+   const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
+ }).catch(()=>caches.match(e.request)));
+});
